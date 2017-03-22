e849170985efd4b8878f77547c85c014496d8659
Anand Nigam <annigam01@hotmail.com>
2017-03-22 13:36:46 +0530

    Add project files.

---
+++ Delegate-AnonymousFunction-method/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_AnonymousFunction_method
+{
+    class Program
+    {
+
+        public delegate int Mydelegate(int a);
+
+        static void Main(string[] args)
+        { // Mydelegate A = new Mydelegate( 
+          //                                   this is inline your function delcaration - 
+          //                                delegate (int a) 
+          //                                {
+          //                                    your method code 
+          //                                        return a * a; 
+          //                                 });
+            Mydelegate A = new Mydelegate(
+                                            delegate (int a) 
+                                            {
+                                                return a * a;
+                                            });
+            //call the delegate
+            Console.WriteLine(A(3));
+        }
+    }
+}
